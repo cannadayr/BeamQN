6dedd5263e27b80aba3fdf2d3ca122ed2ecb1196
Rowan Cannaday <cannadayr@gmail.com>
2024-02-17 18:11:34 -0500

    Free Erlang allocations in same context.

--- c_src/beamqn_nif.c
+++ c_src/beamqn_nif.c
@@ -22,8 +22,14 @@ static ERL_NIF_TERM beamqn_make_error(ErlNifEnv* env, const char* mesg) {
 
 static void beamqn_free_bqnv(ErlNifEnv* env, void* ptr) {
     BQNV* x = (BQNV*) ptr;
+    // CBQN uses its own memory management system (see CBQN/src/opt/) and reads past the end
+    // of allocations (see CBQN/src/opt/mm_malloc.c).
+    // It is unknown if allocations made with enif_alloc allow reading past the end.
+    // If we use the erts allocator, and it does not allow reading past the end, we may
+    // need to allocate an additional 64 bytes on every enif_alloc.
+    // To use the erts allocator, we will need to either add a new allocator to CBQN,
+    // or overload the malloc/free calls with beamqn equivalents.
     bqn_free(*x);
-    enif_free(x);
 }
 
 static int beamqn_init(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM load_info) {
@@ -91,6 +97,7 @@ static ERL_NIF_TERM beamqn_bqn_makeF64Vec(ErlNifEnv* env, int argc, const ERL_NI
 
     term = enif_make_resource(env, bqn_f64Vec);
     enif_release_resource(bqn_f64Vec);
+    enif_free(bqn_arr);
 
     tsdiff = enif_make_int64(env, enif_monotonic_time(ERL_NIF_USEC)-ts0);
 
@@ -141,6 +148,9 @@ static ERL_NIF_TERM beamqn_bqn_readF64Vec(ErlNifEnv* env, int argc, const ERL_NI
         }
 
         term = enif_make_list_from_array(env,ebuf,len);
+
+        enif_free(buf);
+        enif_free(ebuf);
     }
 
     tsdiff = enif_make_int64(env, enif_monotonic_time(ERL_NIF_USEC)-ts0);
